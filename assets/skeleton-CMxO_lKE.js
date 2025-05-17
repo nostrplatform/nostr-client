fc5e0473431a79d487bf79f12aed67c36f9c0d7b
miladsoft <miladsoft@users.noreply.github.com>
2025-05-17 11:35:33 +0000

    deploy: 187b07df71c44411e49820ff41220d371a01acff

---
+++ assets/skeleton-CMxO_lKE.js
@@ -0,0 +1,6 @@
+import{g as o,j as t,n as a,aj as s}from"./index-BETSscnm.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=o("Calendar",[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]]),d=s("inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground hover:bg-primary/80",secondary:"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",destructive:"border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",outline:"text-foreground"}},defaultVariants:{variant:"default"}});function u({className:e,variant:r,...n}){return t.jsx("div",{className:a(d({variant:r}),e),...n})}function f({className:e,...r}){return t.jsx("div",{className:a("animate-pulse rounded-md bg-muted",e),...r})}export{u as B,c as C,f as S};
